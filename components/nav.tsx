df39b936f5eff09a4401c0b09aa7e0519909eb0b
lazywon <jooowon.dev@gmail.com>
2022-10-25 22:45:36 +0900

    feat: add mobile navigation bar

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,17 +1,19 @@
 import Link from "next/link";
 import React from "react";
 import navlinks from "../data/navlinks";
+import MobileNav from "./mobileNav";
 
 const Nav = () => {
   return (
     <nav className="flex relative w-full max-w-2xl items-center justify-between mx-auto">
-      <div>
+      <div className="hidden sm:block">
         {navlinks.map(({ title, link }) => (
           <Link href={link} key={title}>
             <a className="mr-4 font-semibold">{title}</a>
           </Link>
         ))}
       </div>
+      <MobileNav />
     </nav>
   );
 };
